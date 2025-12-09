2a78f411823833b505583394bfe21c02df20c143
Meep0Zero <dev.kcee37340@gmail.com>
2025-12-09 17:23:32 +0800

    Update email contact in Hero component

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -249,7 +249,7 @@ const Hero = () => {
                 Interested in collaborating or have a project in mind?
               </p>
               <div className="contact-form">
-                <a href="mailto:kingcharlie@example.com" className="btn btn-primary btn-block">
+                <a href="mailto:dev.kcee37340@gmail.com" className="btn btn-primary btn-block">
                   <FaEnvelope /> Send Email
                 </a>
                 <p className="form-note">
